urbit/vere

commit 6d077370d1d1ef8ac44e3b9696f0678fd3e74425

vere: when scry crashes, produce fake result of ~
In the long term, we don't want to pretend we got "no result", and
handle crashes by whatever behavior is appropriate.
In the short term, we don't want to burden callsites with crash
handling, so we punt on the problem by producing ~.

pkg/vere/lord.c
@@ -349,6 +349,8 @@ _lord_plea_peek_bail(u3_lord* god_u, u3_peek* pek_u, u3_noun dud)
 {
   u3_pier_punt_goof("peek", dud);
 
+  pek_u->fun_f(pek_u->ptr_v, u3_nul);
+
   u3z(pek_u->now);
   u3z(pek_u->gan);
   u3z(pek_u->ful);

commit 51572809203c33d8d6ff94cb9207f4a4faf26eac

vere: don't crash the process on scry crash
The fake result introduced in 6d077370d isn't very useful if the process
ends up crashing anyway...

pkg/vere/lord.c
@@ -355,8 +355,6 @@ _lord_plea_peek_bail(u3_lord* god_u, u3_peek* pek_u, u3_noun dud)
   u3z(pek_u->gan);
   u3z(pek_u->ful);
   c3_free(pek_u);
-
-  _lord_bail(god_u);
 }
 
 /* _lord_plea_peek_done(): hear serf %peek %done