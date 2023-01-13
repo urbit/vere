92badc0f59ef763ceeb6ab0c0ceae1d238572540
Peter McEvoy <peter@tlon.io>
2023-01-13 12:14:06 -0500

    Fix tracing.

--- pkg/vere/mars.c
+++ pkg/vere/mars.c
@@ -16,10 +16,11 @@ static void
 _mars_step_trace(const c3_c* dir_c)
 {
   if ( u3C.wag_w & u3o_trace ) {
-    if ( u3_Host.tra_u.con_w == 0  && u3_Host.tra_u.fun_w == 0 ) {
+    c3_w trace_cnt_w = u3t_trace_cnt();
+    if ( trace_cnt_w == 0  && u3t_file_cnt() == 0 ) {
       u3t_trace_open(dir_c);
     }
-    else if ( u3_Host.tra_u.con_w >= 100000 ) {
+    else if ( trace_cnt_w >= 100000 ) {
       u3t_trace_close();
       u3t_trace_open(dir_c);
     }
