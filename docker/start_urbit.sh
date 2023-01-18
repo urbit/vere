f54984d6ffd1a4febdd16a6358662308fc5f99dc
Peter McEvoy <git@mcevoypeter.com>
2023-01-17 20:47:26 -0500

    Provide sudo to setcap

--- docker/start_urbit.sh
+++ docker/start_urbit.sh
@@ -17,7 +17,7 @@ esac
 done
 
 # Set network capabilities for the Urbit binary.
-setcap 'cap_net_bind_service=+ep' $(which urbit)
+sudo setcap 'cap_net_bind_service=+ep' $(readlink $(which urbit))
 
 # If the container is not started with the `-i` flag
 # then STDIN will be closed and we need to start
