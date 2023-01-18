4d6f43029d7b420f26338c553a6f22abfa6c2fb4
Peter McEvoy <git@mcevoypeter.com>
2023-01-18 09:02:31 -0500

    Non-interactively reset +code

--- docker/reset_urbit_code.sh
+++ docker/reset_urbit_code.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-resp=$(curl -s -X POST -H "Content-Type: application/json" \
-  -d '{ "source": { "dojo": "+hood/code %reset" }, "sink": { "app": "hood" } }' \
+# Sending `+hood/code %reset` to `dojo` resets the `+code`, but it requires
+# interactive confirmation to do so. Obviously, we don't want to have to provide
+# that confirmation, so we force reset the `+code` using
+# `+hood/pass [%j %step ~]`, credit for which goes to ~wicdev-wisryt.
+resp=$(curl -s -X POST -H "Content-Type: application/json"                            \
+  -d '{ "source": { "dojo": "+hood/pass [%j %step ~]" }, "sink": { "app": "hood" } }' \
   http://127.0.0.1:12321)
 
 if [[ $? -eq 0 ]]
