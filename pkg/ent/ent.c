20ba8de62c0360a706c3e71e2f005b54e32809b8
Peter McEvoy <git@mcevoypeter.com>
2023-01-19 10:38:53 -0500

    Address sys/random.h macOS bug

--- pkg/ent/ent.c
+++ pkg/ent/ent.c
@@ -12,6 +12,9 @@ int ent_getentropy(void* buf, size_t len) {
 
 #elif defined(ENT_GETENTROPY_SYSRANDOM)
 #include <stddef.h>
+// See https://www.mail-archive.com/bug-gnulib@gnu.org/msg38583.html.
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/random.h>
 int ent_getentropy(void* buf, size_t len) {
   return getentropy(buf, len);
