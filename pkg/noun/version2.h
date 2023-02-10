cfba21c00074e1a536c4e94ff3a485cd99e64e9c
barter-simsum <barter-simsum@protonmail.com>
2023-02-10 12:07:38 -0500

    cenums are dumb

--- pkg/noun/version2.h
+++ pkg/noun/version2.h
@@ -4,19 +4,18 @@
 /* VORTEX
  */
 
-typedef enum u3v_version {
-  U3V_VER1   = 1,
-  /* 1 -> 2: 1 bit pointer compression to enable 8G loom */
-  U3V_VER2   = 2,
-  U3V_VERLAT = U3V_VER2,
-} u3v_version;
+typedef c3_w       u3v_version;
+
+#define U3V_VER1   1
+#define U3V_VER2   2
+#define U3V_VERLAT U3V_VER2
 
 /* EVENTS
  */
 
-typedef enum u3e_version {
-  U3E_VER1   = 1,
-  U3E_VERLAT = U3E_VER1,
-} u3e_version;
+typedef c3_w       u3e_version;
+
+#define U3E_VER1   1
+#define U3E_VERLAT U3E_VER1
 
 #endif /* ifndef U3_VERSION2_H */
