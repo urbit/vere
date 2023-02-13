d59093165f52dfd61aec763e2574ace7723a8185
Peter McEvoy <git@mcevoypeter.com>
2023-02-13 13:02:35 -0500

    Stub out functions

---
+++ pkg/pma/pma.c
@@ -0,0 +1,21 @@
+#include "pma.h"
+
+//==============================================================================
+// FUNCTIONS
+//==============================================================================
+
+pma_t *
+pma_init(const char *heap_file, const char *stack_file)
+{
+    return NULL;
+}
+
+int
+pma_sync(pma_t *pma, size_t heap_sz, size_t stack_sz)
+{
+    return 0;
+}
+
+void
+pma_deinit(pma_t *pma)
+{}
