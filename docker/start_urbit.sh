5898b4fd5ffbde102c1b953b7d947d931827635e
mopfel-winrux <amadeo.bellotti@gmail.com>
2023-02-14 10:30:14 -0500

    Updated start-urbit.sh
    Added loom and httpPort for comets :(

--- docker/start_urbit.sh
+++ docker/start_urbit.sh
@@ -55,7 +55,7 @@ elif [ -e *.comet ]; then
   cometname=''${comets[0]}
   rm *.comet
 
-  urbit $ttyflag -c $(basename $cometname .comet) -p $amesPort -x
+  urbit $ttyflag -c $(basename $cometname .comet) -p $amesPort -x --http-port $httpPort --loom $loom
 fi
 
 # Find the first directory and start urbit with the ship therein
@@ -63,4 +63,4 @@ dirnames="*/"
 dirs=( $dirnames )
 dirname=''${dirnames[0]}
 
-exec urbit $ttyflag -p $amesPort --http-port $httpPort  --loom $loom $dirname
+exec urbit $ttyflag -p $amesPort --http-port $httpPort --loom $loom $dirname
