3194118ec1e8d5ad1eaaf128ab2fe65f42930db6
Peter McEvoy <git@mcevoypeter.com>
2023-02-16 12:50:32 -0500

    Stub out read_all() and write_all()

--- pkg/pma/util.c
+++ pkg/pma/util.c
@@ -2,6 +2,9 @@
 
 #include "util.h"
 
+//==============================================================================
+// ARITHMETIC
+
 inline size_t
 max(size_t a, size_t b)
 {
@@ -24,4 +27,19 @@ inline size_t
 round_up(size_t x, size_t n)
 {
     return (x + (n - 1)) & (~(n - 1));
+}
+
+//==============================================================================
+// I/O
+
+int
+read_all(int fd, void *buf, size_t len)
+{
+    return -1;
+}
+
+int
+write_all(int fd, const void *buf, size_t len)
+{
+    return -1;
 }
--- pkg/pma/util.h
+++ pkg/pma/util.h
@@ -5,6 +5,9 @@
 
 #include <stddef.h>
 
+//==============================================================================
+// ARITHMETIC
+
 /// Determine the max of `a` and `b`.
 ///
 /// @param[in] a
@@ -32,5 +35,33 @@ round_down(size_t x, size_t n);
 /// @param[in] n
 extern size_t
 round_up(size_t x, size_t n);
+
+//==============================================================================
+// I/O
+
+/// Read all requested bytes from a file into a buffer.
+///
+/// @param[in] fd    File descriptor to read from.
+/// @param[out] buf  Buffer to read into. Must have enough space for `len`
+///                  bytes.
+/// @param[in] len   Number of bytes to read into `buf`.
+///
+/// @return -1  `len` bytes could not be read into `buf`. `errno` is set
+///             appropriately.
+/// @return  0  `len` bytes read into `buf`.
+int
+read_all(int fd, void *buf, size_t len);
+
+/// Write all requested bytes from a buffer into a file.
+///
+/// @param[in] fd   File descriptor to write to.
+/// @param[in] buf  Buffer to write from. Must have `len` bytes.
+/// @param[in] len  Number of bytes to write from `buf`.
+///
+/// @return -1  `len` bytes could not be written to `fd`. `errno` is set
+///             appropriately.
+/// @return  0  `len` bytes written to `fd`.
+int
+write_all(int fd, const void *buf, size_t len);
 
 #endif /* ifndef PMA_UTIL_H */
