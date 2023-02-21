b1abe834a8f05dbbf997db81ffabdabf5340eb85
Amadeo Bellotti <amadeo.bellotti@gmail.com>
2023-02-21 16:44:33 -0500

    modified workflow for read, red, and turn

--- pkg/vere/io/loch.c
+++ pkg/vere/io/loch.c
@@ -51,27 +51,37 @@ _loch_read_bail(u3_ovum* egg_u, u3_noun lud)
 /* u3_loch_ef_read(): read from device
 */
 static void
-_loch_ef_read(u3_loch* teh_u, u3_noun cmd)
+_loch_ef_read(u3_loch* teh_u, u3_noun wir_i, u3_device dev_d, u3_noun wut, u3_noun cmd)
 {
   if ( c3n == teh_u->car_u.liv_o ) {
     teh_u->car_u.liv_o = c3y;
   }
+  c3_y* buf_y = malloc(cmd*sizeof(c3_y));
+  u3l_log("loch cmd: %d", cmd);
+  u3l_log("loch fid: %d", dev_d.fid_w);
+  
+  read(dev_d.fid_w, buf_y, cmd);
 
+
+  u3_noun red = u3i_bytes(cmd, buf_y);
+  u3l_log("red: %llu", red);
   // extract read count and device from cmd noun
   // preform read on device and store in c3_y*
   // turn that c3_y* into some u3_noun
   // send read event with u3_noun to arvo
-  //
+  //[%turn =dev =act dat=@ud]
+
   {
-    u3_noun wir = u3nc(c3__behn, u3_nul);
-    u3_noun cad = u3nc(c3__wake, u3_nul);
+    u3_noun wir = u3nc(c3__loch, u3_nul);
+    u3_noun dat = u3nt(dev_d.nam_w, c3__read, red);
+    u3_noun cad = u3nc(c3__turn, dat);
 
     u3_auto_peer(
-      u3_auto_plan(&teh_u->car_u, u3_ovum_init(0, c3__b, wir, cad)),
+      u3_auto_plan(&teh_u->car_u, u3_ovum_init(0, c3__l, wir, cad)),
       0, 0, _loch_read_bail);
   }
 
-  u3z(cmd);
+  //u3z(cmd);
 }
 
 /* _loch_born_news(): initialization complete on %born.
@@ -104,9 +114,7 @@ static void
 _loch_io_talk(u3_auto* car_u)
 {
   u3_loch* loc_u = (u3_loch*)car_u;
-  u3_noun  wir = u3nt(c3__loch,
-                      u3dc("scot", c3__uv, 1),
-                      u3_nul);
+  u3_noun  wir = u3nc(c3__loch, u3_nul);
   u3_noun  cad = u3nc(c3__born, u3_nul);
 
   u3_auto_peer(
@@ -139,20 +147,50 @@ _loch_io_kick(u3_auto* car_u, u3_noun wir, u3_noun cad)
 {
   u3_loch* teh_u = (u3_loch*)car_u;
 
-  u3_noun tag, dat, i_wir;
-  c3_o ret_o;
+  u3_noun tag, i_wir, par;
+  u3_noun wut, dev, cmd, dat, tmp;
+  u3_device* dev_d;
+  c3_o ret_o, dev_o;
 
   if (  (c3n == u3r_cell(wir, &i_wir, 0))
-     || (c3n == u3r_cell(cad, &tag, &dat))
-     || (c3__loch != i_wir) 
-     || (c3__read != tag) )
+     || (c3n == u3r_cell(cad, &tag, &par))
+     || (c3__loch != i_wir) )
   {
     ret_o = c3n;
   }
   else {
-    u3l_log("loch: %%kick win;~tiller-tolbus ");
-    ret_o = c3y;
-    _loch_ef_read(teh_u, u3k(dat)); // apply writ command
+    if (  (c3n == u3r_cell(par, &wut, &tmp))
+       || (c3n == u3r_cell(tmp, &dev, &cmd)))
+    {
+      ret_o = c3n;
+    }
+    else {
+      //See if the device is present and open
+      dev_o = c3n;
+      for( c3_w i = 0; i< teh_u->cnt_w; i++ ) {
+        if(  (dev == teh_u->dev_u[i].nam_w)
+          && (c3y == teh_u->dev_u[i].opn_o)) {
+            u3l_log("loch dev found");
+            dev_o = c3y;
+            dev_d = &teh_u->dev_u[i];
+        }
+      }
+      if (c3y == dev_o) {
+        if ( (c3__read == tag) )
+        {
+          u3l_log("loch read");
+          _loch_ef_read(teh_u, wir, *dev_d, wut, cmd); // execute read command
+          ret_o = c3y;
+        }
+        else if ( c3__writ == tag )
+        {
+          u3l_log("loch: writ ");
+        }
+      }
+      else {
+        ret_o = c3n;
+      }
+    }
   }
 
   u3z(wir); u3z(cad);
@@ -171,8 +209,9 @@ _loch_io_exit(u3_auto* car_u)
     if( c3y == teh_u->dev_u[i].opn_o ) {
       close(teh_u->dev_u[i].fid_w);
     }
-    c3_free(&teh_u->dev_u[i]);
+    //c3_free(&teh_u->dev_u[i]);
   }
+  //c3_free(&teh_u->dev_u);
   c3_free(teh_u);
 }
 
@@ -188,7 +227,7 @@ u3_loch_io_init(u3_pier* pir_u)
   u3_device* random = c3_calloc(sizeof(teh_u));
 
   random->nam_w = c3_s4('r','a','n','d');
-  random->fil_c = "/dev/random";
+  random->fil_c = "/dev/urandom";
 
   teh_u->cnt_w = 1;
   teh_u->dev_u = c3_calloc(teh_u->cnt_w*sizeof(teh_u));
@@ -198,7 +237,7 @@ u3_loch_io_init(u3_pier* pir_u)
   for( c3_w i = 0; i< teh_u->cnt_w; i++ ) {
     teh_u->dev_u[i].fid_w = open(teh_u->dev_u[i].fil_c, O_RDWR);
     if ( teh_u->dev_u[i].fid_w  < 0 ) { //If any error report it and keep device marked as close
-      fprintf(stderr, "Unable to open device file %s \n", teh_u->dev_u[i].fil_c);
+      u3l_log("Unable to open device file %s \n", teh_u->dev_u[i].fil_c);
     } else {
       teh_u->dev_u[i].opn_o = c3y;
       teh_u->dev_u[i].tus_w = 0;
