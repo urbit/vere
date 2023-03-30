822c391d414fc47810f1b17f0f6f26d46305fa76
Peter McEvoy <git@mcevoypeter.com>
2023-03-30 11:46:10 -0400

    Add retry logic to read_all()

--- pkg/pma/util.c
+++ pkg/pma/util.c
@@ -29,36 +29,67 @@ round_up(size_t x, size_t n);
 int
 read_all(int fd, void *buf, size_t len)
 {
+    int err;
     if (!buf) {
-        errno = EINVAL;
-        return -1;
+        err = EINVAL;
+        goto fail;
     }
     if (len == 0) {
         return 0;
     }
-    char *ptr = buf;
+    off_t cur_offset = lseek(fd, 0, SEEK_CUR);
+    if (cur_offset == (off_t)-1) {
+        err = errno;
+        goto fail;
+    }
+    static const size_t kMaxAttempts = 100;
+    size_t              attempts     = 0;
+    char               *ptr          = buf;
     do {
+        attempts++;
         ssize_t bytes_read = read(fd, ptr, len);
         if (bytes_read == -1) {
+            if ((err == EINTR || err == EAGAIN || err == EWOULDBLOCK)
+                && attempts <= kMaxAttempts)
+            {
+                // From the read(2) man page: "On error, -1 is returned, and
+                // errno is set to indicate the error. In this case, it is left
+                // unspecified whether the file position (if any) changes."
+                //
+                // This means that we have to restore the file cursor to the
+                // position that it was at immediately before the call to read()
+                // that failed.
+                if (lseek(fd, SEEK_SET, cur_offset) == (off_t)-1) {
+                    err = errno;
+                    goto fail;
+                }
+                continue;
+            }
             fprintf(stderr,
                     "util: failed to read %zu bytes into %p: %s\r\n",
                     len,
                     ptr,
-                    strerror(errno));
-            return -1;
+                    strerror(err));
+            goto fail;
         }
         if (bytes_read == 0) {
+            err = ECANCELED;
             fprintf(stderr,
                     "util: encountered unexpected EOF when reading %zu bytes "
                     "into %p\r\n",
                     len,
                     ptr);
-            return -1;
+            goto fail;
         }
         len -= bytes_read;
         ptr += bytes_read;
+        cur_offset += bytes_read;
     } while (len > 0);
     return 0;
+
+fail:
+    errno = err;
+    return -1;
 }
 
 int
