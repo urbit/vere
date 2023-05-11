urbit/vere

commit a86724f12fcf79ac3048fd10452098834cda0bf1

ames: refactors _fine_scry_path() and its call-site

pkg/vere/io/ames.c
@@ -1537,31 +1537,21 @@ _fine_lop(c3_w fra_w)
 }
 
 static u3_weak
-_fine_scry_path(u3_pact* pac_u, c3_o lop_o)
+_fine_scry_path(u3_pact* pac_u)
 {
-  u3_peep* pep_u = (
-    ( PACT_WAIL == pac_u->typ_y )
-    ? &pac_u->wal_u.pep_u
-    : &pac_u->pur_u.pep_u);
+  u3_peep* pep_u = ( PACT_WAIL == pac_u->typ_y )
+                   ? &pac_u->wal_u.pep_u
+                   : &pac_u->pur_u.pep_u;
+  u3_noun    ful = u3dc("rush", u3i_string(pep_u->pat_c), u3v_wish("stap"));
 
-  u3_noun pat;
-  {
-    u3_noun pux = u3i_string(pep_u->pat_c);
-    u3_noun ful = u3dc("rush", pux, u3v_wish("stap"));
-    if ( u3_nul == ful ) {
-      u3z(ful);
-      return u3_none;
-    }
-    pat = u3k(u3t(ful));
+  if ( u3_nul == ful ) {
+    return u3_none;
+  }
+  else {
+    u3_noun pro = u3k(u3t(ful));
     u3z(ful);
+    return pro;
   }
-
-  c3_w fra_w = pep_u->fra_w;
-  if ( c3y == lop_o ) {
-    fra_w = _fine_lop(fra_w);
-  }
-
-  return u3nc(pat, u3i_word(fra_w));
 }
 
 /* _fine_hunk_scry_cb(): receive packets for datum out of fine
@@ -1634,7 +1624,7 @@ _fine_hear_request(u3_pact* req_u, c3_w cur_w)
 {
   u3_ames* sam_u = req_u->sam_u;
   u3_pact* res_u;
-  u3_weak    key;
+  u3_noun    key;
 
   if ( c3n == _fine_sift_wail(req_u, cur_w) ) {
     sam_u->sat_u.wal_d++;
@@ -1647,14 +1637,20 @@ _fine_hear_request(u3_pact* req_u, c3_w cur_w)
   }
   //  make scry cache key
   //
-  else if ( u3_none == (key = _fine_scry_path(req_u, c3n)) ) {
-    sam_u->sat_u.wap_d++;
-    if ( 0 == (sam_u->sat_u.wap_d % 100) ) {
-      u3l_log("fine: %" PRIu64 " dropped wails (path)",
-              sam_u->sat_u.wap_d);
+  else {
+    u3_weak yek = _fine_scry_path(req_u);
+
+    if ( u3_none == yek  ) {
+      sam_u->sat_u.wap_d++;
+      if ( 0 == (sam_u->sat_u.wap_d % 100) ) {
+        u3l_log("fine: %" PRIu64 " dropped wails (path)",
+                sam_u->sat_u.wap_d);
+      }
+      _ames_pact_free(req_u);
+      return;
     }
-    _ames_pact_free(req_u);
-    return;
+
+    key = yek;
   }
 
   //  fill in the parts of res_u that we know from req_u
@@ -1739,11 +1735,11 @@ _fine_hear_request(u3_pact* req_u, c3_w cur_w)
       u3nq(c3__hunk,
            u3dc("scot", c3__ud, u3i_word(lop_w)),
            u3dc("scot", c3__ud, FINE_PAGE),
-           u3k(u3h(key))));
+           u3k(key)));
 
     //  mark as pending in the scry cache
     //
-    _fine_put_cache(res_u->sam_u, u3h(key), lop_w, FINE_PEND);
+    _fine_put_cache(res_u->sam_u, key, lop_w, FINE_PEND);
 
     //  scry into arvo for a page of packets
     //

commit 975b6249d8b61388016329529243fdf4d41cfb3e

ames: fix fine cache lookup
broken in a86724f12fcf79ac3048fd10452098834cda0bf1

pkg/vere/io/ames.c
@@ -1131,6 +1131,17 @@ _ames_czar(u3_pact* pac_u)
   }
 }
 
+/* _fine_put_cache(): get packet list or status from cache. RETAIN
+ */
+static u3_weak
+_fine_get_cache(u3_ames* sam_u, u3_noun pax, c3_w lop_w)
+{
+  u3_noun key = u3nc(u3k(pax), u3i_word(lop_w));
+  u3_weak pro = u3h_git(sam_u->fin_s.sac_p, key);
+  u3z(key);
+  return pro;
+}
+
 /* _fine_put_cache(): put packet list or status into cache. RETAIN.
  */
 static void
@@ -1730,7 +1741,9 @@ _fine_hear_request(u3_pact* req_u, c3_w cur_w)
 
   //  look up request in scry cache
   //
-  u3_weak cac = u3h_git(res_u->sam_u->fin_s.sac_p, key);
+  c3_w  lop_w = _fine_lop(res_u->pur_u.pep_u.fra_w);
+  u3_weak cac = _fine_get_cache(sam_u, key, lop_w);
+
   //  already pending; drop
   //
   if ( FINE_PEND == cac ) {
@@ -1748,7 +1761,6 @@ _fine_hear_request(u3_pact* req_u, c3_w cur_w)
                                   res_u->pur_u.pep_u.pat_c);
     }
 
-    c3_w lop_w = _fine_lop(res_u->pur_u.pep_u.fra_w);
     u3_noun pax =
       u3nc(c3__fine,
       u3nq(c3__hunk,