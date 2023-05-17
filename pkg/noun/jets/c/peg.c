b1d7d26cf738e73e8a05255af216817f002f0532
Joe Bryan <joemfb@gmail.com>
2023-05-16 23:44:48 -0400

    jets: optimizes direct +peg

--- pkg/noun/jets/c/peg.c
+++ pkg/noun/jets/c/peg.c
@@ -14,21 +14,29 @@
       return u3k(a);
     }
 
-    u3_atom d, e, f, g, h;
     c3_w  c_w = u3r_met(0, b) - 1;
 
-    d = u3i_word(c_w);
-    e = u3qc_lsh(0, d, 1);
-    f = u3qa_sub(b, e);
-    g = u3qc_lsh(0, d, a);
-    h = u3qa_add(f, g);
+    if ( (c3y == u3a_is_cat(a) && (c3y == u3a_is_cat(b))) ) {
+      c3_w d_w = b - (1 << c_w);
+      c3_d e_d = (c3_d)a << c_w;
+      return u3i_chub(d_w + e_d);
+    }
+    else {
+      u3_atom d, e, f, g, h;
 
-    u3z(d);
-    u3z(e);
-    u3z(f);
-    u3z(g);
+      d = u3i_word(c_w);
+      e = u3qc_lsh(0, d, 1);
+      f = u3qa_sub(b, e);
+      g = u3qc_lsh(0, d, a);
+      h = u3qa_add(f, g);
 
-    return h;
+      u3z(d);
+      u3z(e);
+      u3z(f);
+      u3z(g);
+
+      return h;
+    }
   }
   u3_noun
   u3wc_peg(u3_noun cor)
