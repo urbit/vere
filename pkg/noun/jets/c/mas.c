0b114c5c2c151daa6faa8c88a07806510ddf2252
Joe Bryan <joemfb@gmail.com>
2023-05-16 23:45:48 -0400

    jets: restyles +mas

--- pkg/noun/jets/c/mas.c
+++ pkg/noun/jets/c/mas.c
@@ -1,48 +1,46 @@
-/// @file
-
 #include "jets/q.h"
 #include "jets/w.h"
 
 #include "noun.h"
 
+u3_noun
+u3qc_mas(u3_atom a)
+{
+  c3_w b_w;
+  u3_atom c, d, e, f;
 
-  u3_noun
-  u3qc_mas(u3_atom a)
-  {
-    c3_w b_w;
-    u3_atom c, d, e, f;
-
-    b_w = u3r_met(0, a);
-    if ( b_w < 2 ) {
-      return u3m_bail(c3__exit);
-    }
-    else if ( c3y == u3a_is_cat(a) ) {
-      return (a - (1 << (b_w - 1))) | (1 << (b_w - 2));
-    }
-    else {
-      c = u3qc_bex(u3i_word(b_w - 1));
-      d = u3qc_bex(u3i_word(b_w - 2));
-      e = u3qa_sub(a, c);
-      f = u3qc_con(e, d);
-
-      u3z(c);
-      u3z(d);
-      u3z(e);
-
-      return f;
-    }
+  b_w = u3r_met(0, a);
+  if ( b_w < 2 ) {
+    return u3m_bail(c3__exit);
   }
-  u3_noun
-  u3wc_mas(u3_noun cor)
-  {
-    u3_noun a;
-
-    if ( (u3_none == (a = u3r_at(u3x_sam, cor))) ||
-         (c3n == u3ud(a)) )
-    {
-      return u3m_bail(c3__exit);
-    } else {
-      return u3qc_mas(a);
-    }
+  else if ( c3y == u3a_is_cat(a) ) {
+    return (a - (1 << (b_w - 1))) | (1 << (b_w - 2));
   }
+  else {
+    c = u3qc_bex(u3i_word(b_w - 1));
+    d = u3qc_bex(u3i_word(b_w - 2));
+    e = u3qa_sub(a, c);
+    f = u3qc_con(e, d);
 
+    u3z(c);
+    u3z(d);
+    u3z(e);
+
+    return f;
+  }
+}
+
+u3_noun
+u3wc_mas(u3_noun cor)
+{
+  u3_noun a;
+
+  if ( (u3_none == (a = u3r_at(u3x_sam, cor))) ||
+       (c3n == u3ud(a)) )
+  {
+    return u3m_bail(c3__exit);
+  }
+  else {
+    return u3qc_mas(a);
+  }
+}
