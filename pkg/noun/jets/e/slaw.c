aa79a639faa5721328cb79a93ade27d8762d35ea
Mikołaj Paraniak <mikolajp@lux-space.io>
2023-05-25 11:28:43 +0800

    jets: refactors the +slaw jet

--- pkg/noun/jets/e/slaw.c
+++ pkg/noun/jets/e/slaw.c
@@ -8,90 +8,6 @@
 
 #include <ctype.h>
 
-static inline u3_noun
-_parse_da(u3_noun a)
-{
-  u3_weak pro;
-
-  if ( u3_none == (pro = u3s_sift_da(u3x_atom(a))) ) {
-    return u3_nul;
-  }
-
-  return u3nc(u3_nul, pro);
-}
-
-static inline u3_noun
-_parse_p(u3_noun a)
-{
-  u3_weak pro;
-
-  if ( u3_none == (pro = u3s_sift_p(u3x_atom(a))) ) {
-    return u3_nul;
-  }
-
-  return u3nc(u3_nul, pro);
-}
-
-static inline u3_noun
-_parse_ud(u3_noun a)
-{
-  u3_weak pro;
-
-  if ( u3_none == (pro = u3s_sift_ud(u3x_atom(a))) ) {
-    return u3_nul;
-  }
-
-  return u3nc(u3_nul, pro);
-}
-
-static inline u3_noun
-_parse_ui(u3_noun a)
-{
-  u3_weak pro;
-
-  if ( u3_none == (pro = u3s_sift_ui(u3x_atom(a))) ) {
-    return u3_nul;
-  }
-
-  return u3nc(u3_nul, pro);
-}
-
-static inline u3_noun
-_parse_ux(u3_noun a)
-{
-  u3_weak pro;
-
-  if ( u3_none == (pro = u3s_sift_ux(u3x_atom(a))) ) {
-    return u3_nul;
-  }
-
-  return u3nc(u3_nul, pro);
-}
-
-static inline u3_noun
-_parse_uv(u3_noun a)
-{
-  u3_weak pro;
-
-  if ( u3_none == (pro = u3s_sift_uv(u3x_atom(a))) ) {
-    return u3_nul;
-  }
-
-  return u3nc(u3_nul, pro);
-}
-
-static inline u3_noun
-_parse_uw(u3_noun a)
-{
-  u3_weak pro;
-
-  if ( u3_none == (pro = u3s_sift_uw(u3x_atom(a))) ) {
-    return u3_nul;
-  }
-
-  return u3nc(u3_nul, pro);
-}
-
 u3_noun
 _parse_tas(u3_noun txt) {
   // For any symbol which matches, txt will return itself as a
@@ -102,62 +18,76 @@ _parse_tas(u3_noun txt) {
   c3_c* cur = c;
   if (!islower(cur[0])) {
     u3a_free(c);
-    return 0;
+    return u3_none;
   }
   cur++;
 
   while (cur[0] != 0) {
     if (!(islower(cur[0]) || isdigit(cur[0]) || cur[0] == '-')) {
       u3a_free(c);
-      return 0;
+      return u3_none;
     }
 
     cur++;
   }
 
   u3a_free(c);
-  return u3nc(0, u3k(txt));
+  return u3k(txt);
+}
+
+u3_noun
+u3qe_slaw(u3_atom a, u3_atom b)
+{
+  u3_noun res;
+
+  switch(a) {
+
+    case c3__da: res = u3s_sift_da(b); break;
+
+    case 'p': res = u3s_sift_p(b);     break;
+
+    case c3__ud: res = u3s_sift_ud(b); break;
+    case c3__ui: res = u3s_sift_ui(b); break;
+    case c3__uv: res = u3s_sift_uv(b); break;
+    case c3__uw: res = u3s_sift_uw(b); break;
+    case c3__ux: res = u3s_sift_ux(b); break;
+
+    // %ta is used once in link.hoon. don't bother.
+
+    case c3__tas: {
+                      res = _parse_tas(b);
+                      if ( res == u3_none ) {
+                          return u3_nul;
+                      }
+                  }
+
+
+    default: return u3_none;
+  }
+
+  // The u3s_sift functions
+  // signal parsing failure by returning u3_none.
+  // This does not mean the input was wrong - the jet
+  // could simply choose not to handle certain cases.
+  //
+  if ( res == u3_none ) {
+    return u3_none;
+  }
+
+  return u3nc(u3_nul, res);
+
 }
 
 u3_noun
 u3we_slaw(u3_noun cor)
 {
-  u3_noun mod;
-  u3_noun txt;
+  u3_noun a;
+  u3_noun b;
 
-  if (c3n == u3r_mean(cor, u3x_sam_2, &mod,
-                      u3x_sam_3, &txt, 0)) {
+  if (c3n == u3r_mean(cor, u3x_sam_2, &a,
+                      u3x_sam_3, &b, 0)) {
     return u3m_bail(c3__exit);
   }
 
-  switch (mod) {
-    case c3__da:
-      return _parse_da(txt);
-
-    case 'p':
-      return _parse_p(txt);
-
-    case c3__ud:
-      return _parse_ud(txt);
-
-    case c3__ui:
-      return _parse_ui(txt);
-
-    case c3__ux:
-      return _parse_ux(txt);
-
-    case c3__uv:
-      return _parse_uv(txt);
-
-    case c3__uw:
-      return _parse_uw(txt);
-
-      // %ta is used once in link.hoon. don't bother.
-
-    case c3__tas:
-      return _parse_tas(txt);
-
-    default:
-      return u3_none;
-  }
+  return u3qe_slaw(u3x_atom(a), u3x_atom(b));
 }
