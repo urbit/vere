877e94b62c264b8b8001ae0eaecda2b458772b10
Joe Bryan <joemfb@gmail.com>
2023-06-15 18:48:06 -0400

    play: fixes full replay commandline arg parsing

--- pkg/vere/main.c
+++ pkg/vere/main.c
@@ -2000,7 +2000,7 @@ _cw_play(c3_i argc, c3_c* argv[])
     { "no-demand", no_argument,       NULL, 6 },
     { "auto-meld", no_argument,       NULL, 7 },
     { "soft-mugs", no_argument,       NULL, 8 },
-    { "full",      required_argument, NULL, 'f' },
+    { "full",      no_argument,       NULL, 'f' },
     { "replay-to", required_argument, NULL, 'n' },
     { "snap-at",   required_argument, NULL, 's' },
     { NULL, 0, NULL, 0 }
