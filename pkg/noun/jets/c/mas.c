b58e4c7d7d6665bde9edb4877305f71c4595fc3c
Joe Bryan <joemfb@gmail.com>
2023-10-07 17:37:27 -0400

    u3: further optimizes +mas jet

--- pkg/noun/jets/c/mas.c
+++ pkg/noun/jets/c/mas.c
@@ -7,40 +7,43 @@ u3_noun
 u3qc_mas(u3_atom a)
 {
   c3_w b_w;
-  u3_atom c, d, e, f;
 
-  b_w = u3r_met(0, a);
-  if ( b_w < 2 ) {
-    return u3m_bail(c3__exit);
-  }
-  else if ( c3y == u3a_is_cat(a) ) {
-    return (a - (1 << (b_w - 1))) | (1 << (b_w - 2));
+  if ( c3y == u3a_is_cat(a) ) {
+    b_w = c3_bits_word(a);
+
+    if ( 2 > b_w ) {
+      return u3m_bail(c3__exit);
+    }
+    else {
+      a  &= ~((c3_w)1 << (b_w - 1));
+      a  |=  ((c3_w)1 << (b_w - 2));
+      return a;
+    }
   }
   else {
-    c = u3qc_bex(u3i_word(b_w - 1));
-    d = u3qc_bex(u3i_word(b_w - 2));
-    e = u3qa_sub(a, c);
-    f = u3qc_con(e, d);
+    b_w = u3r_met(0, a);
 
-    u3z(c);
-    u3z(d);
-    u3z(e);
+    if ( 64 > b_w ) {
+      c3_d a_d = u3r_chub(0, a);
+      a_d  &= ~((c3_d)1 << (b_w - 1));
+      a_d  |=  ((c3_d)1 << (b_w - 2));
+      return u3i_chub(a_d);
+    }
+    else {
+      u3i_slab sab_u;
+      u3i_slab_from(&sab_u, a, 0, --b_w);
 
-    return f;
+      sab_u.buf_w[(b_w >> 5)] &= ((c3_w)1 << (b_w & 31)) - 1;
+      b_w--;
+      sab_u.buf_w[(b_w >> 5)] |= ((c3_w)1 << (b_w & 31));
+
+      return u3i_slab_mint(&sab_u);
+    }
   }
 }
 
 u3_noun
 u3wc_mas(u3_noun cor)
 {
-  u3_noun a;
-
-  if ( (u3_none == (a = u3r_at(u3x_sam, cor))) ||
-       (c3n == u3ud(a)) )
-  {
-    return u3m_bail(c3__exit);
-  }
-  else {
-    return u3qc_mas(a);
-  }
+  return u3qc_mas(u3x_atom(u3x_at(u3x_sam, cor)));
 }
