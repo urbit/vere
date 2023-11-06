c6dbc603918c9829b53b09aa81e601fa051a078d
pkova <pyry@urbit.org>
2023-11-06 19:28:35 +0200

    ames: rename nal_d to nal_o

--- pkg/vere/io/ames.c
+++ pkg/vere/io/ames.c
@@ -45,7 +45,7 @@
     c3_w             imp_w[256];        //  imperial IPs
     time_t           imp_t[256];        //  imperial IP timestamps
     c3_o             imp_o[256];        //  imperial print status
-    c3_o             nal_d;             //  lane cache backcompat flag
+    c3_o             nal_o;             //  lane cache backcompat flag
     struct {                            //    config:
       c3_o           net_o;             //  can send
       c3_o           see_o;             //  can scry
@@ -1478,7 +1478,7 @@ _ames_try_send(u3_pact* pac_u, c3_o for_o)
   //
   else {
     u3_noun key = u3i_chubs(2, pac_u->pre_u.rec_d);
-    lac = _ames_lane_from_cache(sam_u->lax_p, key, sam_u->nal_d);
+    lac = _ames_lane_from_cache(sam_u->lax_p, key, sam_u->nal_o);
   }
 
   //  if we know there's no lane, drop the packet
@@ -2282,7 +2282,7 @@ _ames_kick_newt(u3_ames* sam_u, u3_noun tag, u3_noun dat)
       u3_noun who = u3k(u3h(dat));
       u3_noun las = u3k(u3t(dat));
       _ames_lane_into_cache(sam_u->lax_p, who, las);
-      sam_u->nal_d = c3y;
+      sam_u->nal_o = c3y;
       ret_o = c3y;
     } break;
   }
@@ -2466,7 +2466,7 @@ u3_ames_io_init(u3_pier* pir_u)
 {
   u3_ames* sam_u  = c3_calloc(sizeof(*sam_u));
   sam_u->pir_u    = pir_u;
-  sam_u->nal_d    = c3n;
+  sam_u->nal_o    = c3n;
   sam_u->fig_u.net_o = c3y;
   sam_u->fig_u.see_o = c3y;
   sam_u->fig_u.fit_o = c3n;
