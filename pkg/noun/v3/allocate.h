efe106650e8da7504b3aa3188875e9c7e06b2826
Joe Bryan <joemfb@gmail.com>
2023-11-09 15:42:34 -0500

    u3: moves u3a_ream (free-list adjustment) into v3 migration

--- pkg/noun/v3/allocate.h
+++ pkg/noun/v3/allocate.h
@@ -11,6 +11,7 @@
 #     define u3R_v3              u3a_Road
 #     define u3a_v3_balign       u3a_balign
 #     define u3a_v3_road         u3a_road
+#     define u3a_v3_ream         u3a_ream
 #     define u3a_v3_walign       u3a_walign
 #     define u3a_v3_walloc       u3a_walloc
 
