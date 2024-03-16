40f015d5a8a39726ff442fd6c519853816a47209
Joe Bryan <joemfb@gmail.com>
2024-03-15 23:00:39 -0400

    xmas: validate mugs while sifting packets

--- pkg/vere/io/xmas/pact.c
+++ pkg/vere/io/xmas/pact.c
@@ -588,6 +588,17 @@ xmas_sift_pact(u3_xmas_pact* pac_u, c3_y* buf_y, c3_w len_w)
       break;
     }
   }
+
+  {
+    c3_w mug_w = u3r_mug_bytes(buf_y, res_w);
+    mug_w &= 0xFFFFF;
+
+    if ( mug_w != pac_u->hed_u.mug_w ) {
+      u3l_log("xmas: failed mug");
+      return 0;
+    }
+  }
+
   //u3_assert(res_w <= len_w );
   return res_w + 8;
 }
