bf88fad227f5dc60a5f9bb441ebf7f16a32b926b
Joe Bryan <joemfb@gmail.com>
2024-04-18 10:05:32 -0400

    ames: adds new batch czar dns

--- pkg/vere/io/ames.c
+++ pkg/vere/io/ames.c
@@ -24,6 +24,8 @@
 
 #define QUEUE_MAX        30             //  max number of packets in queue
 
+#define _CZAR_GONE  UINT32_MAX
+
 typedef enum u3_stun_state {
   STUN_OFF = 0,
   STUN_TRYING = 1,
@@ -58,6 +60,14 @@ typedef enum u3_stun_state {
     c3_w             imp_w[256];        //  imperial IPs
     time_t           imp_t[256];        //  imperial IP timestamps
     c3_o             imp_o[256];        //  imperial print status
+    struct {                            //
+      c3_c       dom_c[251];            //    domain
+      c3_o       dom_o;                 //    have domain
+      uv_timer_t tim_u;                 //    resolve timer
+      c3_s       pen_s;                 //    pending
+      c3_w       pip_w[256];            //    ipv4
+      c3_w       log_w[256 >> 5];       //    log error
+    } zar_u;                            //
     struct {                            //    stun client state:
       u3_stun_state  sat_y;             //  formal state
       c3_y           tid_y[12];         //  last transaction id
@@ -1026,6 +1036,41 @@ _ames_czar_port(c3_y imp_y)
   else {
     return 13337 + imp_y;
   }
+}
+
+/* _ames_czar_str: galaxy name as c3_c[3]
+*/
+static void
+_ames_czar_str(c3_c zar_c[3], c3_y imp_y)
+{
+  u3_po_to_suffix(imp_y, (c3_y*)zar_c, (c3_y*)zar_c + 1, (c3_y*)zar_c + 2);
+}
+
+/* _ames_etch_czar: galaxy fqdn
+*/
+static c3_i
+_ames_etch_czar(c3_c dns_c[256], const c3_c* dom_c, c3_y imp_y)
+{
+  c3_c* bas_c = dns_c;
+  c3_w  len_w = strlen(dom_c);
+
+  //  name 3, '.' 2, trailing null
+  //
+  if ( 250 <= len_w ) {
+    return -1;
+  }
+
+  _ames_czar_str(dns_c, imp_y);
+  dns_c   += 3;
+  *dns_c++ = '.';
+
+  memcpy(dns_c, dom_c, len_w);
+  dns_c   += len_w;
+  *dns_c++ = '.';
+
+  memset(dns_c, 0, 256 - (dns_c - bas_c));
+
+  return 0;
 }
 
 static c3_c*
@@ -2894,34 +2939,187 @@ _ames_io_start(u3_ames* sam_u)
   u3z(who);
 }
 
+typedef struct _czar_resv {
+  uv_getaddrinfo_t adr_u;
+  u3_ames*         sam_u;
+  c3_y             imp_y;
+} _czar_resv;
+
+/* _ames_czar2_gone(): galaxy address resolution failed.
+*/
+static void
+_ames_czar2_gone(u3_ames* sam_u, c3_y imp_y)
+{
+  c3_w old_w = sam_u->zar_u.pip_w[imp_y];
+
+  if ( !old_w ) {
+    sam_u->zar_u.pip_w[imp_y] = _CZAR_GONE;
+  }
+}
+
+/* _ames_czar2_here(): galaxy address resolution succeeded.
+*/
+static void
+_ames_czar2_here(u3_ames* sam_u, c3_y imp_y, c3_w pip_w)
+{
+  c3_w old_w = sam_u->zar_u.pip_w[imp_y];
+
+  if ( pip_w != old_w ) {
+    c3_c dns_c[256];
+    u3_assert ( !_ames_etch_czar(dns_c, sam_u->zar_u.dom_c, imp_y) );
+    u3l_log("ames: czar %s ip .%d.%d.%d.%d", dns_c,
+            (pip_w >> 24) & 0xff,
+            (pip_w >> 16) & 0xff,
+            (pip_w >>  8) & 0xff,
+            (pip_w >>  0) & 0xff);
+  }
+
+  sam_u->zar_u.pip_w[imp_y] = imp_y;
+
+  {
+    c3_w blk_w = imp_y >> 5;
+    c3_w bit_w = 1 << (imp_y & 31);
+
+    sam_u->zar_u.log_w[blk_w] &= ~bit_w;
+  }
+}
+
+/* _ames_czar2_cb(): galaxy address resolution callback.
+*/
+static void
+_ames_czar2_cb(uv_getaddrinfo_t* adr_u,
+               c3_i              sas_i,
+               struct addrinfo*  aif_u)
+{
+  struct addrinfo* rai_u = aif_u;
+  _czar_resv*      res_u = (_czar_resv*)adr_u;
+  u3_ames*         sam_u = res_u->sam_u;
+  c3_y             imp_y = res_u->imp_y;
+
+  while ( rai_u && (AF_INET != rai_u->ai_family) ) {
+    rai_u = rai_u->ai_next;
+  }
+
+  if ( rai_u && rai_u->ai_addr ) {
+    struct sockaddr_in* add_u = (void*)rai_u->ai_addr;
+    c3_w pip_w = ntohl(add_u->sin_addr.s_addr);
+    _ames_czar2_here(sam_u, imp_y, pip_w);
+  }
+  else {
+    if ( !sas_i ) {
+      // XX unpossible
+      u3l_log("ames: czar: strange failure, no error");
+    }
+    else if ( u3C.wag_w & u3o_verbose ) {
+      u3l_log("ames: czar fail: %s", uv_strerror(sas_i));
+    }
+
+    _ames_czar2_gone(sam_u, imp_y);
+  }
+
+  uv_freeaddrinfo(aif_u);
+  c3_free(res_u);
+}
+
+/* _ames_czar2(): single galaxy address resolution.
+*/
+static void
+_ames_czar2(u3_ames* sam_u, const c3_c* dom_c, c3_y imp_y)
+{
+  struct addrinfo   hin_u = { .ai_family = AF_INET };
+  uv_getaddrinfo_t* adr_u;
+  _czar_resv*       res_u;
+  c3_c              dns_c[256];
+  c3_i              sas_i;
+
+  u3_assert ( !_ames_etch_czar(dns_c, dom_c, imp_y) );
+
+  res_u = c3_malloc(sizeof(*res_u));
+  res_u->sam_u = sam_u;
+  res_u->imp_y = imp_y;
+
+  adr_u = &(res_u->adr_u);
+  sas_i = uv_getaddrinfo(u3L, adr_u, _ames_czar2_cb, dns_c, 0, &hin_u);
+
+  if ( sas_i ) {
+    _ames_czar2_cb(adr_u, sas_i, NULL);
+  }
+}
+
+/* _ames_czar_all(): galaxy address resolution.
+*/
+static void
+_ames_czar_all(uv_timer_t* tim_u)
+{
+  u3_ames* sam_u = tim_u->data;
+
+  //  requests still pending
+  if ( sam_u->zar_u.pen_s ) {
+    uv_timer_start(&sam_u->zar_u.tim_u, _ames_czar_all, 30*1000, 0);
+    return;
+  }
+
+  sam_u->zar_u.pen_s = 256;
+
+  for ( c3_w i_w; i_w < 256; i_w++ ) {
+    _ames_czar2(sam_u, sam_u->zar_u.dom_c, (c3_y)i_w);
+  }
+
+  uv_timer_start(&sam_u->zar_u.tim_u, _ames_czar_all, 300*1000, 0);
+}
+
 /* _ames_ef_turf(): initialize ames I/O on domain(s).
 */
 static void
 _ames_ef_turf(u3_ames* sam_u, u3_noun tuf)
 {
   if ( u3_nul != tuf ) {
-    //  XX save all for fallback, not just first
-    //
-    u3_noun hot = u3k(u3h(tuf));
+    c3_c  dom_c[sizeof(sam_u->zar_u.dom_c)];
+    u3_noun hot = u3h(tuf);
     c3_w  len_w = u3_mcut_host(0, 0, u3k(hot));
 
-    sam_u->dns_c = c3_malloc(1 + len_w);
-    u3_mcut_host(sam_u->dns_c, 0, hot);
-    sam_u->dns_c[len_w] = 0;
-
-    if ( 250 <= len_w ) {
+    if ( len_w >= sizeof(dom_c) ) {  // >250
       //  3 char for the galaxy (e.g. zod) and two dots
-      u3l_log("ames: galaxy domain too big %s len=%u", sam_u->dns_c, len_w);
+      u3l_log("ames: galaxy domain too big (len=%u)", len_w);
+      u3m_p("hot", hot);
       u3_pier_bail(u3_king_stub());
     }
 
-    //  XX invalidate sam_u->imp_w &c ?
-    c3_free(sam_u->sun_u.dns_c);
-    sam_u->sun_u.dns_c = 0;
+    u3_mcut_host(dom_c, 0, u3k(hot));
+    memset(dom_c + len_w, 0, sizeof(dom_c) - len_w);
+
+    if ( 0 != memcmp(sam_u->zar_u.dom_c, dom_c, sizeof(dom_c)) ) {
+      //  XX legacy
+      //
+      sam_u->dns_c = c3_malloc(1 + len_w);
+      memcpy(sam_u->dns_c, dom_c, len_w);
+      sam_u->dns_c[len_w] = 0;
+
+      //  XX review
+      //
+      c3_free(sam_u->sun_u.dns_c);
+      sam_u->sun_u.dns_c = 0;
+
+      memcpy(sam_u->zar_u.dom_c, dom_c, sizeof(dom_c));
+      memset(sam_u->zar_u.pip_w, 0, sizeof(sam_u->zar_u.pip_w));
+      sam_u->zar_u.dom_o = c3y;
+      _ames_czar_all(&(sam_u->zar_u.tim_u));
+    }
+
+    //  XX save all for fallback, not just first
+    //
+    if ( u3_nul != u3t(tuf) ) {
+      u3l_log("ames: turf: ignoring additional domains");
+      u3m_p("second", u3h(u3t(tuf)));
+
+      if ( u3_nul != u3t(u3t(tuf)) ) {
+        u3m_p("third", u3h(u3t(u3t(tuf))));
+      }
+    }
 
     u3z(tuf);
   }
-  else if ( (c3n == sam_u->pir_u->fak_o) && (0 == sam_u->dns_c) ) {
+  else if ( (c3n == sam_u->pir_u->fak_o) && (c3y == sam_u->zar_u.dom_o) ) {
     u3l_log("ames: turf: no domains");
   }
 
@@ -3221,6 +3419,9 @@ u3_ames_io_init(u3_pier* pir_u)
   sam_u->fig_u.net_o = c3y;
   sam_u->fig_u.see_o = c3y;
   sam_u->fig_u.fit_o = c3n;
+
+  uv_timer_init(u3L, &sam_u->zar_u.tim_u);
+  sam_u->zar_u.tim_u.data = sam_u;
 
   //  initialize STUN timers
   uv_timer_init(u3L, &sam_u->sun_u.dns_u);
