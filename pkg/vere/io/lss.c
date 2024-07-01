08c3992e4b553ee6cb7e5391055289978dd1e794
lukechampine <luke.champine@gmail.com>
2024-07-01 13:49:07 -0400

    lss: add lss_builder

--- pkg/vere/io/lss.c
+++ pkg/vere/io/lss.c
@@ -33,7 +33,7 @@ static void _parent_hash(lss_hash out, lss_hash left, lss_hash right)
   memcpy(out, block, 32);
 }
 
-static c3_y bits_ctz64(c3_d val_d)
+static c3_y _bits_ctz64(c3_d val_d)
 {
   if (val_d == 0 ) {
     return 64;
@@ -48,7 +48,7 @@ static c3_y bits_ctz64(c3_d val_d)
   return ret_y;
 }
 
-static c3_y bits_len64(c3_d val_d)
+static c3_y _bits_len64(c3_d val_d)
 {
   if (val_d == 0) {
     return 0;
@@ -63,13 +63,87 @@ static c3_y bits_len64(c3_d val_d)
   return ret_y;
 }
 
-static c3_o lss_find_pair(lss_verifier* los_u, c3_w height, c3_b sel, lss_hash h)
+c3_w lss_proof_size(c3_w leaves) {
+  return 1 + _bits_len64(leaves-1);
+}
+
+c3_o _lss_expect_pair(c3_w leaves, c3_w i) {
+  if ( ((i != 0) && (i + (1 << (1+_bits_ctz64(i))) < leaves)) ) {
+    return c3y;
+  }
+  return c3n;
+}
+
+static void _lss_builder_merge(lss_builder* bil_u, c3_w height, lss_hash l, lss_hash r) {
+  // whenever two subtrees are merged, insert them into the pairs array;
+  // if the merged tree is part of the left-side "spine" of the tree,
+  // instead add the right subtree to the initial proof
+  if (height+1 < _bits_len64(bil_u->counter)) {
+    c3_w c = (bil_u->counter&~((1<<(height+1))-1)) - (1<<height);
+    memcpy(bil_u->pairs[c][0], l, sizeof(lss_hash));
+    memcpy(bil_u->pairs[c][1], r, sizeof(lss_hash));
+  } else {
+    if (height == 0) {
+      memcpy(bil_u->proof[0], l, sizeof(lss_hash)); // proof always begins with [0, 1)
+    }
+    memcpy(bil_u->proof[height+1], r, sizeof(lss_hash));
+  }
+}
+
+void lss_builder_ingest(lss_builder* bil_u, c3_y* leaf_y, c3_w leaf_w) {
+  lss_hash h;
+  _leaf_hash(h, leaf_y, leaf_w, bil_u->counter);
+  c3_w height = 0;
+  while ( bil_u->counter&(1<<height) ) {
+    _lss_builder_merge(bil_u, height, bil_u->trees[height], h);
+    _parent_hash(h, bil_u->trees[height], h);
+    height++;
+  }
+  memcpy(bil_u->trees[height], h, sizeof(lss_hash));
+  bil_u->counter++;
+}
+
+lss_hash* lss_builder_finalize(lss_builder* bil_u) {
+  if ( bil_u->counter != 0 ) {
+    c3_w height = _bits_ctz64(bil_u->counter);
+    lss_hash h;
+    memcpy(h, bil_u->trees[height], sizeof(lss_hash));
+    for (height++; height < 32; height++) {
+      if ( bil_u->counter&(1<<height) ) {
+        _lss_builder_merge(bil_u, height, bil_u->trees[height], h);
+        _parent_hash(h, bil_u->trees[height], h);
+      }
+    }
+  }
+  return bil_u->proof;
+}
+
+lss_pair* lss_builder_pair(lss_builder* bil_u, c3_w i) {
+  if ( c3y == _lss_expect_pair(bil_u->counter, i) ) {
+    return &bil_u->pairs[i];
+  }
+  return NULL;
+}
+
+void lss_builder_init(lss_builder* bil_u, c3_w leaves) {
+  bil_u->counter = 0;
+  bil_u->proof = c3_calloc(lss_proof_size(leaves) * sizeof(lss_hash));
+  bil_u->pairs = c3_calloc(leaves * sizeof(lss_pair));
+}
+
+void lss_builder_free(lss_builder* bil_u) {
+  c3_free(bil_u->proof);
+  c3_free(bil_u->pairs);
+  c3_free(bil_u);
+}
+
+static c3_o _lss_verifier_find_pair(lss_verifier* los_u, c3_w height, c3_b sel, lss_hash h)
 {
   if (memcmp(los_u->pairs[height][sel], h, 32) == 0) {
     return c3y;
   }
   // look for "irregular" pairs, which will always be on the right
-  c3_w max_height = bits_len64(los_u->leaves);
+  c3_w max_height = _bits_len64(los_u->leaves);
   for (height++; height < max_height; height++) {
     if (memcmp(los_u->pairs[height][1], h, 32) == 0) {
       return c3y;
@@ -80,14 +154,14 @@ static c3_o lss_find_pair(lss_verifier* los_u, c3_w height, c3_b sel, lss_hash h
 
 c3_o lss_verifier_ingest(lss_verifier* los_u, c3_y* leaf_y, c3_w leaf_w, lss_pair* pair) {
   // verify leaf
-  lss_hash leaf_hash;
-  _leaf_hash(leaf_hash, leaf_y, leaf_w, los_u->counter);
-  if ( c3n == lss_find_pair(los_u, 0, los_u->counter&1, leaf_hash) ) {
+  lss_hash h;
+  _leaf_hash(h, leaf_y, leaf_w, los_u->counter);
+  if ( c3n == _lss_verifier_find_pair(los_u, 0, los_u->counter&1, h) ) {
     return c3n;
   }
   // check whether pair is expected
-  c3_w height = bits_ctz64(los_u->counter) + 1;
-  if ((pair != NULL) != ((los_u->counter != 0) && (los_u->counter + (1 << height) < los_u->leaves))) {
+  c3_w height = _bits_ctz64(los_u->counter) + 1;
+  if ( (pair != NULL) != (c3y == _lss_expect_pair(los_u->leaves, los_u->counter)) ) {
     return c3n;
   } else if (pair == NULL) {
     los_u->counter++;
@@ -97,7 +171,7 @@ c3_o lss_verifier_ingest(lss_verifier* los_u, c3_y* leaf_y, c3_w leaf_w, lss_pai
   c3_b sel = ~(los_u->counter >> height) & 1;
   lss_hash parent_hash;
   _parent_hash(parent_hash, (*pair)[0], (*pair)[1]);
-  if ( c3n == lss_find_pair(los_u, height, sel, parent_hash) ) {
+  if ( c3n == _lss_verifier_find_pair(los_u, height, sel, parent_hash) ) {
     return c3n;
   }
   memcpy(los_u->pairs[height-1][0], (*pair)[0], sizeof(lss_hash));
@@ -106,10 +180,11 @@ c3_o lss_verifier_ingest(lss_verifier* los_u, c3_y* leaf_y, c3_w leaf_w, lss_pai
   return c3y;
 }
 
-c3_o lss_verifier_init(lss_verifier* los_u, c3_w leaves, lss_hash* proof, c3_w proof_w) {
+c3_o lss_verifier_init(lss_verifier* los_u, c3_w leaves, lss_hash* proof) {
+  c3_w proof_w = lss_proof_size(leaves);
+  c3_w pairs_w = _bits_len64(leaves);
   los_u->leaves = leaves;
   los_u->counter = 0;
-  c3_w pairs_w = bits_len64(leaves);
   los_u->pairs = c3_calloc(pairs_w * sizeof(lss_pair));
   memcpy(los_u->pairs[0][0], proof[0], sizeof(lss_hash));
   for (c3_w i = 1; i < proof_w; i++) {
@@ -136,17 +211,17 @@ void lss_root(lss_hash root, lss_hash* proof, c3_w proof_w) {
 
 #ifdef LSS_TEST
 
-static void _test_bits_ctz64()
+static void _test__bits_ctz64()
 {
-  #define asrt(x,y) if ( x != y ) { fprintf(stderr, "failed (line: %u) got %u expected %u", __LINE__, x,y); exit(1); }
-  asrt(bits_ctz64(0ULL), 64);
-  asrt(bits_ctz64(0x0000000000000001ULL), 0);
-  asrt(bits_ctz64(0x0000000000000002ULL), 1);
-  asrt(bits_ctz64(0x0000000000000100ULL), 8);
-  asrt(bits_ctz64(0x0000000080000000ULL), 31);
-  asrt(bits_ctz64(0x0000000100000000ULL), 32);
-  asrt(bits_ctz64(0x4000000000000000ULL), 62);
-  asrt(bits_ctz64(0x8000000000000000ULL), 63);
+  #define asrt(x,y) if ( x != y ) { fprintf(stderr, "failed at %s:%u: got %u, expected %u", __FILE__, __LINE__, x,y); exit(1); }
+  asrt(_bits_ctz64(0ULL), 64);
+  asrt(_bits_ctz64(0x0000000000000001ULL), 0);
+  asrt(_bits_ctz64(0x0000000000000002ULL), 1);
+  asrt(_bits_ctz64(0x0000000000000100ULL), 8);
+  asrt(_bits_ctz64(0x0000000080000000ULL), 31);
+  asrt(_bits_ctz64(0x0000000100000000ULL), 32);
+  asrt(_bits_ctz64(0x4000000000000000ULL), 62);
+  asrt(_bits_ctz64(0x8000000000000000ULL), 63);
   #undef asrt
 }
 
@@ -158,9 +233,9 @@ static lss_pair* _make_pair(lss_hash left, lss_hash right)
   return pair;
 }
 
-static void _test_lss_8()
+static void _test_lss_manual_verify_8()
 {
-  #define asrt_ok(y) if ( c3y != y ) { fprintf(stderr, "failed (line: %u)", __LINE__); exit(1); }
+  #define asrt_ok(y) if ( c3y != y ) { fprintf(stderr, "failed at %s:%u\n", __FILE__, __LINE__); exit(1); }
 
   c3_w dat_w = 1024 * 7 + 1;
   c3_y* dat_y = c3_calloc(dat_w);
@@ -197,25 +272,56 @@ static void _test_lss_8()
   // verify
   lss_verifier lss_u;
   memset(&lss_u, 0, sizeof(lss_verifier));
-  asrt_ok(lss_verifier_init(&lss_u, 8, proof, 4))
-  // for ( c3_w i = 0; i < 8; i++ ) {
-  //   asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[i], leaves_w[i], pairs[i]))
-  // }
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[0], leaves_w[0], pairs[0]))
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[1], leaves_w[1], pairs[1]))
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[2], leaves_w[2], pairs[2]))
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[3], leaves_w[3], pairs[3]))
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[4], leaves_w[4], pairs[4]))
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[5], leaves_w[5], pairs[5]))
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[6], leaves_w[6], pairs[6]))
-  asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[7], leaves_w[7], pairs[7]))
+  asrt_ok(lss_verifier_init(&lss_u, 8, proof))
+  for ( c3_w i = 0; i < 8; i++ ) {
+    asrt_ok(lss_verifier_ingest(&lss_u, leaves_y[i], leaves_w[i], pairs[i]))
+  }
+
+  #undef asrt_ok
+}
+
+static void _test_lss_build_verify(c3_w dat_w)
+{
+  #define asrt_ok(y) if ( c3y != y ) { fprintf(stderr, "failed at %s:%u\n", __FILE__, __LINE__); exit(1); }
+
+  c3_y* dat_y = c3_calloc(dat_w);
+  for ( c3_w i = 0; i < dat_w; i++ ) {
+    dat_y[i] = i;
+  }
+  c3_w leaves_w = (dat_w + 1023) / 1024;
+
+  // build
+  lss_builder bil_u;
+  lss_builder_init(&bil_u, leaves_w);
+  for ( c3_w i = 0; i < leaves_w; i++ ) {
+    c3_y* leaf_y = dat_y + (i*1024);
+    c3_w leaf_w = (i < leaves_w - 1) ? 1024 : dat_w % 1024;
+    lss_builder_ingest(&bil_u, leaf_y, leaf_w);
+  }
+  lss_hash* proof = lss_builder_finalize(&bil_u);
+
+  // verify
+  lss_verifier lss_u;
+  asrt_ok(lss_verifier_init(&lss_u, leaves_w, proof))
+  for ( c3_w i = 0; i < leaves_w; i++ ) {
+    c3_y* leaf_y = dat_y + (i*1024);
+    c3_w leaf_w = (i < leaves_w - 1) ? 1024 : dat_w % 1024;
+    lss_pair* pair = lss_builder_pair(&bil_u, i);
+    asrt_ok(lss_verifier_ingest(&lss_u, leaf_y, leaf_w, pair));
+  }
 
   #undef asrt_ok
 }
 
 int main() {
-  _test_bits_ctz64();
-  _test_lss_8();
+  _test__bits_ctz64();
+  _test_lss_manual_verify_8();
+  _test_lss_build_verify(2 * 1024);
+  _test_lss_build_verify(2 * 1024 + 2);
+  _test_lss_build_verify(3 * 1024);
+  _test_lss_build_verify(5 * 1024 + 5);
+  _test_lss_build_verify(17 * 1024 + 512);
+  _test_lss_build_verify(128 * 1024);
   return 0;
 }
 
