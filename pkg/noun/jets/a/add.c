f7d40a8b66a158c9051c4ca5c0e0615c1957ebd6
Quodss <afoninkir170697@gmail.com>
2024-08-11 10:45:49 +0200

    ++add punt on wrong type

--- pkg/noun/jets/a/add.c
+++ pkg/noun/jets/a/add.c
@@ -6,7 +6,7 @@
 
 #include "noun.h"
 
-u3_noun
+u3_atom
 u3qa_add(u3_atom a,
          u3_atom b)
 {
@@ -31,16 +31,14 @@ u3qa_add(u3_atom a,
   }
 }
 
-u3_noun
+u3_weak
 u3wa_add(u3_noun cor)
 {
   u3_noun a, b;
 
-  if ( (c3n == u3r_mean(cor, u3x_sam_2, &a, u3x_sam_3, &b, 0)) ||
-       (c3n == u3ud(a)) ||
-       (c3n == u3ud(b) && a != 0) )
-  {
-    return u3m_bail(c3__exit);
+  u3x_mean(cor, u3x_sam_2, &a, u3x_sam_3, &b, 0);
+  if ( !_(u3ud(a)) || !_(u3ud(b)) ) {
+    return u3_none;
   }
   else {
     return u3qa_add(a, b);
