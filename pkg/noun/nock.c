023ddad9268efa5d444e5326143c8dc27688d6bd
mopfel-winrux <amadeo@nativeplanet.io>
2025-02-01 12:27:18 -0500

    started slow hint stuff

--- pkg/noun/nock.c
+++ pkg/noun/nock.c
@@ -1067,6 +1067,7 @@ _n_bint(u3_noun* ops, u3_noun hif, u3_noun nef, c3_o los_o, c3_o tel_o)
           case c3__meme:
           case c3__nara:
           case c3__hela:
+          case c3__slow:
           case c3__bout: {
             u3_noun fen = u3_nul;
             c3_w  nef_w = _n_comp(&fen, nef, los_o, c3n);
@@ -1931,6 +1932,13 @@ _n_hint_fore(u3_cell hin, u3_noun bus, u3_noun* clu)
       *clu = u3nt(u3k(tag), *clu, now);
     } break;
 
+    case c3__slow: {
+
+      //u3t_slog_cap(1, u3i_string("slow"), hin);
+      u3z(*clu);
+      *clu = c3__slow;
+    } break;
+
     case c3__nara: {
       u3_noun pri, tan;
       if ( c3y == u3r_cell(*clu, &pri, &tan) ) {
@@ -1992,7 +2000,11 @@ static void
 _n_hint_hind(u3_noun tok, u3_noun pro)
 {
   u3_noun p_tok, q_tok, r_tok;
-  if ( (c3y == u3r_trel(tok, &p_tok, &q_tok, &r_tok)) && (c3__bout == p_tok) ) {
+  if (c3__slow == tok) {
+    u3t_slog_nara(0);
+    //u3m_timer_pop();
+  }
+  else if ( (c3y == u3r_trel(tok, &p_tok, &q_tok, &r_tok)) && (c3__bout == p_tok) ) {
     // get the microseconds elapsed
     u3_atom delta = u3ka_sub(u3i_chub(u3t_trace_time()), u3k(r_tok));
 
