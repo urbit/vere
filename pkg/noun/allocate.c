d75a9c5a5291ce1f8b3b419408d789dab53a04b6
Joe Bryan <joemfb@gmail.com>
2025-03-26 10:06:42 -0400

    u3: palloc: refactors mark and sweep to use a separate page directory

--- pkg/noun/allocate.c
+++ pkg/noun/allocate.c
@@ -98,7 +98,7 @@ u3a_init_mark(void)
 
   u3a_Mark.bit_w = c3_calloc(sizeof(c3_w) * bit_w);
   u3a_Mark.siz_w = u3R->hep.siz_w * 2;
-  u3a_Mark.len_w = 0;
+  u3a_Mark.len_w = u3R->hep.len_w;
   u3a_Mark.buf_w = c3_malloc(sizeof(c3_w) * u3a_Mark.siz_w);
 
   memset(u3a_Mark.wee_w, 0, sizeof(c3_w) * u3a_crag_no);
@@ -109,12 +109,8 @@ u3a_mark_alloc(c3_w len_w) // words
 {
   void* ptr_v;
 
-  if ( len_w > u3a_Mark.siz_w ) {
-    u3a_Mark.siz_w += len_w;
-    u3a_Mark.buf_w  = c3_realloc(u3a_Mark.buf_w, sizeof(c3_w) * u3a_Mark.siz_w);
-  }
-  else if ( (u3a_Mark.siz_w - len_w) < u3a_Mark.len_w ) {
-    u3a_Mark.siz_w *= 2;
+  if ( len_w > (u3a_Mark.siz_w - u3a_Mark.len_w) ) {
+    u3a_Mark.siz_w += c3_max(u3a_Mark.len_w, len_w);
     u3a_Mark.buf_w  = c3_realloc(u3a_Mark.buf_w, sizeof(c3_w) * u3a_Mark.siz_w);
   }
 
