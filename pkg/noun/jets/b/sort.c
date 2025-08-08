cccde8c16e780a71e13b14d2c3a54f89c3f8afdc
Quodss <afoninkir170697@gmail.com>
2025-08-08 23:35:56 +0400

    jets: use merge sort for stability

--- pkg/noun/jets/b/sort.c
+++ pkg/noun/jets/b/sort.c
@@ -6,84 +6,94 @@
 #include "noun.h"
 
 
-#define SWAP(l, r)    \
-  do { typeof(l) t = l; l = r; r = t; } while (0)
-
-static void
-_quicksort(u3j_site* sit_u, u3_noun* arr, c3_ws low_ws, c3_ws hig_ws)
-{
-  if ( low_ws >= hig_ws ) return;
-  
-  u3_noun pivot = arr[hig_ws];
-  c3_ws i_ws = low_ws;
-  
-  for (c3_ws j_ws = low_ws; j_ws < hig_ws; j_ws++) {
-    u3_noun hoz = u3j_gate_slam(sit_u, u3nc(u3k(arr[j_ws]), u3k(pivot)));
-    
-    if (hoz > 1) u3m_bail(c3__exit);
-    if ( _(hoz) ) {
-      SWAP(arr[i_ws], arr[j_ws]);
-      i_ws++;
-    }
-  }
-
-  SWAP(arr[i_ws], arr[hig_ws]);
-
-  _quicksort(sit_u, arr, low_ws, i_ws - 1);
-  _quicksort(sit_u, arr, i_ws + 1, hig_ws);
-}
-
-#undef SWAP
-
-static_assert(
-  (UINT32_MAX < (SIZE_MAX / sizeof(u3_noun))),
-  "len_w * sizeof u3_noun overflow"
-);
-
 static_assert( (UINT32_MAX > u3a_cells),
                "length precision" );
 
-//  RETAINS list, transfer product
+static_assert(
+  (UINT32_MAX < (SIZE_MAX / (2 * sizeof(u3_noun)))),
+  "len_w * sizeof u3_noun overflow"
+);
+
+static void
+_merge_sort(u3_noun* arr_u,
+  u3_noun* tmp_u,
+  c3_w lef_w,
+  c3_w rit_w,
+  u3j_site* sit_u)
+{
+  if ( lef_w >= rit_w ) return;
+  c3_w mid_w =  (lef_w + rit_w) / 2;
+  if (mid_w < lef_w)
+  {
+    //  addition wrapped around
+    //
+    u3m_bail(c3__fail);
+  }
+  _merge_sort(arr_u, tmp_u, lef_w, mid_w, sit_u);
+  _merge_sort(arr_u, tmp_u, mid_w + 1, rit_w, sit_u);
+
+  c3_w i_w = lef_w, j_w = mid_w + 1, k_w = lef_w;
+  while (i_w <= mid_w && j_w <= rit_w)
+  {
+    //  reversed comparison to mimick order reversal of pivot
+    //  and compared element in Hoon
+    //
+    u3_noun sam = u3nc(u3k(arr_u[j_w]), u3k(arr_u[i_w]));
+    u3_noun hoz = u3j_gate_slam(sit_u, sam);
+    if ( hoz > 1 ) u3m_bail(c3__exit);
+    if ( c3n == hoz )
+      tmp_u[k_w++] = arr_u[i_w++];
+    else
+      tmp_u[k_w++] = arr_u[j_w++];
+  }
+
+  while (i_w <= mid_w) tmp_u[k_w++] = arr_u[i_w++];
+  while (j_w <= rit_w) tmp_u[k_w++] = arr_u[j_w++];
+
+  for (i_w = lef_w; i_w <= rit_w; i_w++)
+  {
+    arr_u[i_w] = tmp_u[i_w];
+  }
+}
+
+//  RETAINS list, transfers product
 //
 static u3_noun
 _sort(u3j_site* sit_u, u3_noun list)
 {
-  if (u3_nul == list) {
-    return u3_nul;
-  }
-  
+  if (u3_nul == list) return u3_nul;
+
   c3_w len_w = 1;
   {
-    u3_noun lit = u3t(list);
-    while ( u3_nul != lit ) {
-      ++len_w; lit = u3t(lit);
+    u3_noun t = u3t(list);
+    while (u3_nul != t)
+    {
+      ++len_w; t = u3t(t);
     }
   }
 
-  if (1 == len_w) {
-    return u3k(list);
-  }
-
-  c3_w i_w;
-  u3a_cell* cel_u;
-  u3_noun* elements = u3a_malloc(sizeof(u3_noun) * len_w);
-  for (i_w = 0; i_w < len_w; i_w++) {
+  if (1 == len_w) return u3k(list);
+  u3_noun* arr_u = u3a_malloc(sizeof(u3_noun) * len_w * 2);
+  u3_noun* tmp_u = arr_u + len_w;
+  for (c3_w i_w = 0; i_w < len_w; i_w++)
+  {
     //  inlined u3r_cell without any checks
     //  since the list was already validated and measured
     //
-    cel_u = u3a_to_ptr(list);
-    elements[i_w] = cel_u->hed;
+    u3a_cell* cel_u = u3a_to_ptr(list);
+    arr_u[i_w] = cel_u->hed;
     list = cel_u->tel;
   }
 
-  _quicksort(sit_u, elements, 0, len_w - 1);
+  _merge_sort(arr_u, tmp_u, 0, len_w - 1, sit_u);
 
   u3_noun pro = u3_nul;
-  for (i_w = len_w; i_w--;) {
-    pro = u3nc(u3k(elements[i_w]), pro);
+  for (c3_w i_w = len_w; i_w--;)
+  {
+    pro = u3nc(u3k(arr_u[i_w]), pro);
   }
   
-  u3a_free(elements);
+  u3a_free(arr_u);
 
   return pro;
 }
@@ -99,14 +109,18 @@ u3qb_sort(u3_noun a,
   u3j_gate_lose(&sit_u);
   return pro;
 }
+
 u3_noun
 u3wb_sort(u3_noun cor)
 {
   u3_noun a, b;
 
-  if ( c3n == u3r_mean(cor, u3x_sam_2, &a, u3x_sam_3, &b, 0) ) {
+  if ( c3n == u3r_mean(cor, u3x_sam_2, &a, u3x_sam_3, &b, 0) )
+  {
     return u3m_bail(c3__exit);
-  } else {
+  }
+  else
+  {
     return u3qb_sort(a, b);
   }
 }
