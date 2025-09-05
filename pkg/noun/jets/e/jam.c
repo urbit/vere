7a3a7d741565c70e89d1212588861cd876d72928
Matthew LeVan <matthew@coeli.network>
2025-09-05 10:21:06 -0400

    jets: use `c3_n` in `jam`

--- pkg/noun/jets/e/jam.c
+++ pkg/noun/jets/e/jam.c
@@ -9,34 +9,34 @@
 u3_noun
 u3qe_jam(u3_atom a)
 {
-#if 0
+#if 1
   if (c3y == u3du(a) && 1337 == u3h(a)) {
-    c3_w_tmp siz_w, tot_w = 0;
+    c3_n siz_n, tot_n = 0;
     u3_noun som;
     for ( som = u3t(a); c3y == u3du(som); som = u3t(som) ) {
-      siz_w = u3a_count_noun(u3h(som));
-      tot_w += siz_w;
-      if ( 0 == siz_w ) {
+      siz_n = u3a_count_noun(u3h(som));
+      tot_n += siz_n;
+      if ( 0 == siz_n ) {
         u3l_log("item: B/0");
       }
       else {
-        u3a_print_memory(stderr, "item", siz_w);
+        u3a_print_memory(stderr, "item", siz_n);
       }
     }
     if ( u3_blip != som ) {
       u3l_log("forgot to terminate list!");
     }
-    c3_w_tmp mem_w = u3h_count(u3R->cax.har_p);
+    c3_n mem_n = u3h_count(u3R->cax.har_p);
 
     for ( som = u3t(a); c3y == u3du(som); som = u3t(som) ) u3a_discount_noun(u3h(som));
     u3h_discount(u3R->cax.har_p);
 
-    u3a_print_memory(stderr, "total", tot_w);
-    u3a_print_memory(stderr, "memoization cache", mem_w);
+    u3a_print_memory(stderr, "total", tot_n);
+    u3a_print_memory(stderr, "memoization cache", mem_n);
     u3h_root* har_u = u3to(u3h_root, u3R->cax.har_p);
-    u3l_log("memoization entries: %d", har_u->use_w);
+    u3l_log("memoization entries: %llu", har_u->use_w);
 		u3a_print_memory(stderr, "unused free", u3a_open(u3R));
-    return tot_w;
+    return tot_n;
   }
 #endif
 
