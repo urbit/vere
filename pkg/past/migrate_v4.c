731f69ea003f1a0327f10d6870ee545aa6d45c8c
Joe Bryan <joemfb@gmail.com>
2025-09-24 15:13:18 -0400

    past: check for stale snapshot in each migration

--- pkg/past/migrate_v4.c
+++ pkg/past/migrate_v4.c
@@ -7,9 +7,16 @@
 /* u3_migrate_v4: perform loom migration if necessary.
 */
 void
-u3_migrate_v4(void)
+u3_migrate_v4(c3_d eve_d)
 {
   u3_v3_load(u3C.wor_i);
+
+  if ( eve_d != u3H_v3->arv_u.eve_d ) {
+    fprintf(stderr, "loom: migrate (v4) stale snapshot: have %"
+                    PRIu64 ", need %" PRIu64 "\r\n",
+                    u3H_v3->arv_u.eve_d, eve_d);
+    abort();
+  }
 
   fprintf(stderr, "loom: bytecode alignment migration running...\r\n");
 
--- pkg/vere/disk.c
+++ pkg/vere/disk.c
@@ -1656,21 +1656,17 @@ _disk_migrate_loom(c3_c* dir_c, c3_d eve_d)
   c3_i fid_i = _disk_load_stale_loom(dir_c, (size_t)1 << u3_Host.ops_u.lom_y); // XX confirm
   c3_w lom_w = *(u3_Loom_v4 + u3C.wor_i - 1);
 
-  //  For later: check u3A->eve_d against eve_d before migrating
-  //
-  (void)eve_d;
-
   //  NB: all fallthru, all the time
   //
   switch ( lom_w ) {
-    case U3V_VER1: u3_migrate_v2();
-    case U3V_VER2: u3_migrate_v3();
-    case U3V_VER3: u3_migrate_v4();
+    case U3V_VER1: u3_migrate_v2(eve_d);
+    case U3V_VER2: u3_migrate_v3(eve_d);
+    case U3V_VER3: u3_migrate_v4(eve_d);
     case U3V_VER4: {
       u3m_init((size_t)1 << u3_Host.ops_u.lom_y);
       u3e_live(c3n, strdup(dir_c));
       u3m_pave(c3y);
-      u3_migrate_v5();
+      u3_migrate_v5(eve_d);
       u3m_save();
     }
   }
