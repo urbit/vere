2e91f0e3b3ef135c517bfe29b9fb043ed81787ee
Quodss <afoninkir170697@gmail.com>
2025-10-22 12:18:12 +0200

    update ska_core

--- pkg/noun/ska_core.c
+++ pkg/noun/ska_core.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28c29bba907b5e380808cd90cb122b4ca50b7fa73525740fab704c3acaa172d7
-size 937831
+oid sha256:ca84191da697c3e80197a50b7aad5043f124951e5688bfe57d01e581130dbd1d
+size 870553
