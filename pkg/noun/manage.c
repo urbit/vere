ff8a2df1fa064fe958ad8e7098d6377e1f423b76
Quodss <afoninkir170697@gmail.com>
2025-11-25 17:38:36 +0100

    %drop hint to free ford cache

--- pkg/noun/manage.c
+++ pkg/noun/manage.c
@@ -1337,6 +1337,8 @@ u3m_love(u3_noun pro)
   u3p(u3h_root) per_p = u3R->cax.per_p;
   u3p(u3h_root) for_p = u3R->cax.for_p;
 
+  c3_w fag_w = u3R->how.fag_w;
+
   //  are there any timers on the road?
   //
   c3_o tim_o = u3du(u3R->tim);
@@ -1353,7 +1355,9 @@ u3m_love(u3_noun pro)
   jed_u = u3j_take(jed_u);
   byc_p = u3n_take(byc_p);
   per_p = u3h_take(per_p);
-  for_p = u3h_take(for_p);
+  if ( !(fag_w & u3a_flag_drop) ) {
+    for_p = u3h_take(for_p);
+  }
 
   //  pop the stack
   //
@@ -1366,7 +1370,16 @@ u3m_love(u3_noun pro)
   u3j_reap(jed_u);
   u3n_reap(byc_p);
   u3z_reap(u3z_memo_keep, per_p);
-  u3z_reap(u3z_memo_ford, for_p);
+  if ( !(fag_w & u3a_flag_drop) ) {
+    u3z_reap(u3z_memo_ford, for_p);
+  }
+
+  if ( (fag_w & u3a_flag_drop) ) {
+    u3h_free(u3R->cax.for_p);
+    u3R->cax.for_p = u3h_new_cache(u3C.per_w);
+  }
+
+  u3R->how.fag_w |= (fag_w & u3a_flag_drop);
 
   return pro;
 }
@@ -1511,6 +1524,10 @@ u3m_soft_top(c3_w    mil_w,                     //  timer ms
   /* Free the argument.
   */
   u3z(arg);
+
+  /* Clean up the flags
+  */
+  u3R->how.fag_w = 0;
 
   /* Return the product.
   */
