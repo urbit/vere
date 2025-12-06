b1b44af7ff6022bc2f53588d26bbd695a033562b
Quodss <afoninkir170697@gmail.com>
2025-12-06 23:03:17 +0100

    jets: fix sort jet

--- pkg/noun/jets/b/sort.c
+++ pkg/noun/jets/b/sort.c
@@ -15,44 +15,35 @@ static_assert(
 );
 
 static void
-_merge_sort(u3_noun* restrict arr_u,
-  u3_noun* restrict tmp_u,
-  c3_w lef_w,
-  c3_w rit_w,
-  u3j_site* sit_u)
+_sort_part(u3_noun* restrict arr_u, c3_w len_w, u3j_site* sit_u)
 {
-  if ( lef_w >= rit_w ) return;
-  c3_w mid_w =  (lef_w + rit_w) / 2;
-  if (mid_w < lef_w)
+  if ( len_w <= 1 ) return;
+  u3_noun pivot = arr_u[0];
+  u3_noun* first_u = u3a_malloc(sizeof(u3_noun) * (len_w - 1) * 2);
+  u3_noun* second_u = first_u + (len_w - 1);
+  c3_w f_w = 0, s_w = 0;
+  for (c3_w i_w = 1; i_w < len_w; i_w++)
   {
-    //  addition wrapped around
-    //
-    u3m_bail(c3__fail);
-  }
-  _merge_sort(arr_u, tmp_u, lef_w, mid_w, sit_u);
-  _merge_sort(arr_u, tmp_u, mid_w + 1, rit_w, sit_u);
-
-  c3_w i_w = lef_w, j_w = mid_w + 1, k_w = lef_w;
-  while (i_w <= mid_w && j_w <= rit_w)
-  {
-    //  reversed comparison to mimick order reversal of pivot
-    //  and compared element in Hoon
-    //
-    u3_noun sam = u3nc(u3k(arr_u[j_w]), u3k(arr_u[i_w]));
+    u3_noun sam = u3nc(u3k(arr_u[i_w]), u3k(pivot));
     c3_o hoz_o = u3x_loob(u3j_gate_slam(sit_u, sam));
-    if ( c3n == hoz_o )
-      tmp_u[k_w++] = arr_u[i_w++];
+    if ( _(hoz_o) )
+    {
+      first_u[f_w++] = arr_u[i_w];
+    }
     else
-      tmp_u[k_w++] = arr_u[j_w++];
+    {
+      second_u[s_w++] = arr_u[i_w];
+    }
   }
 
-  while (i_w <= mid_w) tmp_u[k_w++] = arr_u[i_w++];
-  while (j_w <= rit_w) tmp_u[k_w++] = arr_u[j_w++];
+  _sort_part(first_u, f_w, sit_u);
+  _sort_part(second_u, s_w, sit_u);
 
-  for (i_w = lef_w; i_w <= rit_w; i_w++)
-  {
-    arr_u[i_w] = tmp_u[i_w];
-  }
+  arr_u[f_w] = pivot;
+  memcpy(arr_u,           first_u,  f_w * sizeof(u3_noun));
+  memcpy(arr_u + f_w + 1, second_u, s_w * sizeof(u3_noun));
+  
+  u3a_free(first_u);
 }
 
 //  RETAINS list, transfers product
@@ -72,8 +63,7 @@ _sort(u3j_site* sit_u, u3_noun list)
   }
 
   if (1 == len_w) return u3k(list);
-  u3_noun* arr_u = u3a_malloc(sizeof(u3_noun) * len_w * 2);
-  u3_noun* tmp_u = arr_u + len_w;
+  u3_noun* arr_u = u3a_malloc(sizeof(u3_noun) * len_w);
   for (c3_w i_w = 0; i_w < len_w; i_w++)
   {
     //  inlined u3r_cell without any checks
@@ -84,7 +74,7 @@ _sort(u3j_site* sit_u, u3_noun list)
     list = cel_u->tel;
   }
 
-  _merge_sort(arr_u, tmp_u, 0, len_w - 1, sit_u);
+  _sort_part(arr_u, len_w, sit_u);
 
   u3_noun pro = u3_nul;
   for (c3_w i_w = len_w; i_w--;)
