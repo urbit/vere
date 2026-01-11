d51de8a91385c961bedcdf51d583e04a9e2f2806
Quodss <afoninkir170697@gmail.com>
2026-01-11 11:07:42 +0100

    manage: only copy out the stacktrace on unwind

--- pkg/noun/manage.c
+++ pkg/noun/manage.c
@@ -285,7 +285,14 @@ _cm_stack_unwind(void)
   u3_noun tax;
 
   while ( u3R != &(u3H->rod_u) ) {
-    u3_noun yat = u3m_love(u3R->bug.tax);
+    u3_noun yat = u3R->bug.tax;
+    u3m_fall();
+    yat = u3a_take(yat);
+    //  pop the stack
+    //
+    u3a_drop_heap(u3R->cap_p, u3R->ear_p);
+    u3R->cap_p = u3R->ear_p;
+    u3R->ear_p = 0;
 
     u3R->bug.tax = u3kb_weld(yat, u3R->bug.tax);
   }
