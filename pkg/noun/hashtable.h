5310b823c1968aa7cbc874625cc459ac51bc5a1c
dozreg-toplud <afonin.k.u@gmail.com>
2026-03-22 15:21:47 +0100

    cell tree

--- pkg/noun/hashtable.h
+++ pkg/noun/hashtable.h
@@ -4,48 +4,32 @@
 #include "c3/c3.h"
 #include "types.h"
 
-    /**  Data structures
-    ***
-    ***  There are two kinds of hashtables that we need: those that get promoted
-    ***  to senior roads, and those who don't. The former need to be traversable
-    ***  at all times (due to async signal stuff), the latter can be simpler.
-    ***
-    ***  Both can be implemented as simple hash tables with arrays of key-value
-    ***  pairs, with the only difference that the simpler hashtable can have its
-    ***  key-hash-value splat into a struct for better cache coherency, while
-    ***  the "atomic" one will have to store them in a separately allocated
-    ***  structure, pointer to which is stored in the array. In practice it
-    ***  means using cells as such structs.
-    ***
-    ***  This hashtable implementation is inspired by the one in Jai module
-    ***  shipped with the compiler.
-    **/
-    #define u3h_size_min    262144  // must be a power of two
-    #define u3h_slot_free  0
-    #define u3h_slot_tomb  1
-    #define u3h_trim_threshold 40
-    #define u3h_grow_threshold 70
-
     typedef c3_w u3h_slot;
-    #define u3h_slot_warm_flag      ((u3h_slot)1 << 31)
-    #define u3h_slot_be_warm(sot)   ((sot) | u3h_slot_warm_flag)
-    #define u3h_slot_be_cold(sot)   ((sot) & ~u3h_slot_warm_flag)
-    #define u3h_slot_is_warm(sot)   ((sot) >> 31 ? c3y : c3n)
-    #define u3h_slot_to_noun(sot)   (u3h_slot_be_warm(sot))
+
+    #define u3h_slot_warm_flag    ((u3h_slot)1 << 30)
+    #define u3h_slot_kev_mask     ((u3h_slot)1 << 31)
+    #define u3h_slot_is_kev(sot)  ((sot) >> 31 ? c3n : c3y)
+    #define u3h_kev_is_warm(sot)  (((sot) & u3h_slot_warm_flag) ? c3y : c3n)
+    #define u3h_kev_be_warm(sot)  ((sot) | u3h_slot_warm_flag)
+    #define u3h_kev_be_cold(sot)  ((sot) & ~u3h_slot_warm_flag)
+    #define u3h_kev_to_slot(kev)  ((kev) & ~u3h_slot_kev_mask)
+    #define u3h_slot_to_noun(sot) ((sot) | u3h_slot_kev_mask | u3h_slot_warm_flag)
+
+    /* Tree layout:
+
+      A slot is either 0 or a key-value pair "kev" or a pair of two
+      slots, or a list of key-value pairs at the very bottom of the tree.
+
+      Given a mug hash, starting from least significant bits, we go either left
+      or right, until we either reach a "kev" or exhaust all bits, reaching a
+      list of kevs.
+
+    */
 
     typedef struct {
-      c3_w          use_w;  // number of valid items
-      c3_w          loc_w;  // number of allocated slots
-      c3_w          max_w;  // bounded size if max_w > 0
-      c3_w          fil_w;  // number of filled slots (live + tombstone)
-      c3_w          arm_w;  // reclamation clock arm
-      c3_w          red_w;
-      c3_w          rit_w;
-      struct {
-        c3_w        red_w;
-        c3_w        rit_w;
-      } col;
-      u3p(u3h_slot) sot_p;
+      c3_w use_w;
+      c3_w max_w;
+      u3h_slot sot_w;
     } u3h_root;
 
     /**  Functions.
