53a82ff29de7fd7e220214efda69713296ae97d1
dozreg-toplud <afonin.k.u@gmail.com>
2026-06-10 21:33:58 +0200

    jets: faster by_gas

--- pkg/noun/jets/d/by_gas.c
+++ pkg/noun/jets/d/by_gas.c
@@ -6,6 +6,51 @@
 
 #include "noun.h"
 
+//  RETAINS lit
+//
+static u3_noun
+_malt_fast(u3_noun lit)
+{
+  if ( u3_nul == lit ) return u3_nul;
+
+  u3_noun root = u3h(lit);
+  u3_noun rest = u3t(lit);
+  while ( u3_nul != rest ) {
+    u3_noun h = u3h(rest);
+    rest = u3t(rest);
+    //  this will pick last pair, given equal keys
+    if ( _(u3qc_mor(u3h(h), u3h(root))) ) {
+      root = h;
+    }
+  }
+  u3_noun part_l, part_r;
+  u3_noun *cur_l = &part_l, *cur_r = &part_r, *old, *item;
+  rest = lit;
+  while ( u3_nul != rest ) {
+    u3_noun h = u3h(rest);
+    rest = u3t(rest);
+    if ( c3y == u3r_sing(u3h(h), u3h(root)) ) continue;
+    if ( _(u3qc_gor(u3h(h), u3h(root))) ) {
+      old = cur_l;
+      *old = u3i_defcons(&item, &cur_l);
+      *item = u3k(h);
+    }
+    else {
+      old = cur_r;
+      *old = u3i_defcons(&item, &cur_r);
+      *item = u3k(h);
+    }
+  }
+  *cur_l = u3_nul;
+  *cur_r = u3_nul;
+
+  u3_noun l = _malt_fast(part_l);
+  u3_noun r = _malt_fast(part_r);
+  u3_noun out = u3nt(u3k(root), l, r);
+  u3z(part_l); u3z(part_r);
+  return out;
+}
+
 u3_noun
 u3qdb_gas(u3_noun a,
           u3_noun b)
@@ -14,6 +59,9 @@ u3qdb_gas(u3_noun a,
     return u3k(a);
   }
   else {
+    if ( u3_nul == a ) {
+      return _malt_fast(b);
+    }
     u3_noun i_b,  t_b,
             pi_b, qi_b;
     u3x_cell(b, &i_b, &t_b);
