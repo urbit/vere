ff3942d63354144bd74c3a4e6d621288beb3eb3f
dozreg-toplud <afonin.k.u@gmail.com>
2026-07-29 17:57:29 +0200

    http: actually unlink first item; return early

--- pkg/vere/io/http.c
+++ pkg/vere/io/http.c
@@ -1900,7 +1900,7 @@ _http_serv_unlink(u3_http* htp_u)
   u3_http* pre_u = htp_u->htd_u->htp_u;
 
   if ( pre_u == htp_u ) {
-    pre_u = htp_u->nex_u;
+    htp_u->htd_u->htp_u = htp_u->nex_u;
   }
   else {
     //  XX glories of linear search
@@ -1908,6 +1908,7 @@ _http_serv_unlink(u3_http* htp_u)
     while ( pre_u ) {
       if ( pre_u->nex_u == htp_u ) {
         pre_u->nex_u = htp_u->nex_u;
+        return;
       }
       else pre_u = pre_u->nex_u;
     }
