2801661b66e621435c887dc47cf42bbae9c75aa1
dozreg-toplud <afonin.k.u@gmail.com>
2026-08-05 17:24:58 +0200

    allocate: rounding without magic constants

--- pkg/noun/allocate.c
+++ pkg/noun/allocate.c
@@ -258,8 +258,8 @@ u3a_pile_prep(u3a_pile* pil_u, c3_w len_w, c3_w alg_w)
 {
   //  frame size, in words
   //
-  c3_w wor_w = (len_w + 3) >> 2;
-  c3_w wal_w = alg_w >> 2;
+  c3_w wor_w = (len_w + sizeof(c3_w) - 1) / sizeof(c3_w);
+  c3_w wal_w = alg_w / sizeof(c3_w);
   c3_o nor_o = u3a_is_north(u3R);
 
   pil_u->mov_ws = (c3y == nor_o) ? -wor_w :  wor_w;
