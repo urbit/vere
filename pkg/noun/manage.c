de95b92dafc24eea004062a428039583b8e81ee8
~dozreg-toplud <kirill@urbit.org>
2026-09-08 19:09:16 +0200

    road: change sizeof to a manual offset computation

--- pkg/noun/manage.c
+++ pkg/noun/manage.c
@@ -651,7 +651,8 @@ _find_home(void)
     //  bytes between the end of the u3v_home struct and the bottom of the heap
     //  should normally be set to 0.
     c3_w* byt_w = (c3_w*)u3_Loom;
-    for (c3_w i_w = sizeof(u3v_home); i_w < ((c3_w)1) << u3a_page; i_w++) {
+    c3_w off_w = offsetof(u3v_home, rod_u.cax.for_p) + sizeof(u3v_Home->rod_u.cax.for_p);
+    for (c3_w i_w = off_w; i_w < ((c3_w)1) << u3a_page; i_w++) {
       u3_assert(!byt_w[i_w] && "loom: downgrade detected");
     }
   }
