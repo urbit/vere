a1cc1018d8a8631755e43a3dce25f1001979418a
~dozreg-toplud <kirill@urbit.org>
2026-09-11 13:57:04 +0200

    noun: dedup the product in nock too

--- pkg/noun/nock.c
+++ pkg/noun/nock.c
@@ -2794,12 +2794,12 @@ _n_burn(u3n_prog* pog_u, u3_noun bus, c3_ys mov, c3_ys off)
       top = _n_peek(off);
       o   = *top;
       if ( u3z_memo_ford == u3h(o) && ( 0 == u3R->ski.gul ) ) {
-        u3z_save_m(u3h(o), 136 + c3__ford, u3t(o), x);
+        x = u3z_save_m_dedup(u3h(o), 136 + c3__ford, u3t(o), x);
       }
       else if ( ( u3z_memo_toss == u3h(o) )
          ? ( &(u3H->rod_u) != u3R )
          : ( 0 == u3R->ski.gul ) ) {  //  prevents persistent memoization when scrying is avaliable
-        u3z_save_m(u3h(o), 144 + c3__nock, u3t(o), x);
+        x = u3z_save_m_dedup(u3h(o), 144 + c3__nock, u3t(o), x);
       }
       // XX can we still print?
       // else if ( u3z_memo_keep == u3h(o) ) {
--- pkg/noun/zave.c
+++ pkg/noun/zave.c
@@ -145,13 +145,23 @@ u3_noun
 u3z_save_m(u3z_cid cid, c3_m fun, u3_noun one, u3_noun val)
 {
   u3_noun key = u3nc(fun, u3k(one));
-  u3_noun val_save = u3k(val);
+  u3h_put(_har(u3R, cid), key, u3k(val));
+  u3z(key);
+  return val;
+}
+
+/* u3z_save_m_dedup(): save in memo cache, deduplicate. All but val is retained.
+*/
+u3_noun
+u3z_save_m_dedup(u3z_cid cid, c3_m fun, u3_noun one, u3_noun val)
+{
+  u3_noun key = u3nc(fun, u3k(one));
   if ( u3z_memo_toss != cid ) {
     key = u3m_dedup(key);
-    val_save = u3m_dedup(val_save);
+    val = u3m_dedup(val);
   }
 
-  u3h_put(_har(u3R, cid), key, val_save);
+  u3h_put(_har(u3R, cid), key, u3k(val));
   u3z(key);
   return val;
 }
